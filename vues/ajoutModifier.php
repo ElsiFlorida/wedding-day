4900fcce0871f4dea7c16b09c7fd70d2d9dff440
Souka_Alice <soukaalice@gmail.com>
2020-05-04 15:30:15 +0000

    vue liste

---
+++ vues/ajoutModifier.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<?php
+include('../header.php');
+?>
+<body>
+<table>
+<tr class="colonne">
+<td>Gestion des Evenements</td>
+<td>Utilisateur</td>
+<td>Liste des Evenements</td>
+</tr>
+</table>
+</body>
+</html>
---
+++ vues/liste.vue.php
@@ -0,0 +1,28 @@
+
+<!DOCTYPE html>
+<html>
+<?php
+include('../header.php');
+?>
+<body>
+<table>
+<tr class="colonne">
+<td>Gestion des Evenements</td>
+<td>Utilisateur</td>
+<td>Liste des Evenements</td>
+</tr>
+<tr class="ligne">
+<td>Nom</td>
+<td>Date debut</td>
+<td>Date fin</td>
+<td>Option</td>
+</tr>
+<tr class="ligne">
+<td></td>
+<td></td>
+<td></td>
+<td>Supprimer Modifier</td>
+</tr>
+</table>
+</body>
+</html>
