89cbe2a8d0557106a6ef37d25696036f8efa29ce
spartiate226 <59071983+spartiate226@users.noreply.github.com>
2020-05-08 14:35:06 +0100

    modification de la vue user

--- vues/user.vue.php
+++ vues/user.vue.php
@@ -1,19 +1,19 @@
 <!DOCTYPE html>
 <html class="h-100">
-        <?php include('../header.php')?>
+        <?php include('header.php')?>
     <body class="h-100">
       <div class="container-fluid">
         <section >
-            <form class="row">
-                <input type="text" class=" flex-grow-1 m-3">
-                <input type="text" class=" flex-grow-1 m-3">
-                <input type="submit" class=" flex-grow-1 m-3">
+            <form action="" method="post" class="row">
+                <input type="text" name="new_pass" class="flex-grow-1 m-3">
+                <input type="text" name="old_pass" class="flex-grow-1 m-3">
+                <input type="submit" value="changer" class="flex-grow-1 m-3">
             </form>
         </section>
         <div class="row">
            <section class="col-md-4">
-           <form class="form whell">
-                <input type="text" class=" form-control m-1">
+           <form action="" method="post" class="form whell">
+                <input type="text" name="username" class=" form-control m-1">
                 <input type="submit" class=" form-control m-1">
             </form>
            </section>
@@ -25,10 +25,16 @@
                        <th>Option</th>
                    </thead>
                    <tbody>
+                   <?php foreach ($liste as $key) {
+                       ?>
                        <tr class="">
-                           <td class="">OUOMPEBA justin</td>
-                           <td class=""><a>X</a></td>
+                           <td class=""><?php echo $key['username'] ?></td>
+                           <td class=""><a href="index.php?page=user&suppr=<?php echo $key['id'] ?>">X</a></td>
                        </tr>
+
+                       <?php
+                       }
+                       ?>
                    </tbody>
                </table>
            </section>
