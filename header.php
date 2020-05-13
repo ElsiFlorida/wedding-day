d694580a6652ae23feb0c017dd348db789667378
spartiate226 <59071983+spartiate226@users.noreply.github.com>
2020-05-13 03:36:23 +0100

    delete master

--- header.php
+++
@@ -1,9 +0,0 @@
-<head>
-   <title>
-      Wedding Day
-   </title>
-   <meta charset="utf-8">
-   <link rel="stylesheet" href="style/fontawesome/css/font-awesome.min.css">
-   <link rel="stylesheet" href="style/bootstrap.min.css">
-   <link rel="stylesheet" href="style/style.css">
-</head>
--- index.php
+++
@@ -1,8 +0,0 @@
-<?php
-if(!isset($_GET['page'])){
-$_GET['page']='accueil';
-}
-include_once('controleurs/router.controller.php');
-$router=new Router();
-$router->request();
-?>
